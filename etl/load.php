625c6dc97baa2be93b105cd8a3809b753c285845
milenastadelmann <milena.stadelmann@stud.fhgr.ch>
2025-05-05 10:36:53 +0200

    Datenbankverbindung hat endlich geklappt

--- etl/load.php
+++ etl/load.php
@@ -13,6 +13,13 @@
 require_once("db_config.php");
 echo "This script receives HTTP POST messages and pushes their content into the database.";
 
+$inputJSON = file_get_contents('php://input');
+file_put_contents("debug.txt", $inputJSON); // Debug-Datei erstellen
+$input = json_decode($inputJSON, true);
+
+if (json_last_error() === JSON_ERROR_NONE) {
+    file_put_contents("debug.txt", "Dekodiert: " . print_r($input, true), FILE_APPEND);
+}
 
 ###################################### connect to db
 try{
@@ -34,23 +41,23 @@ $input = json_decode($inputJSON, true); // Dekodieren der JSON-Daten in ein Arra
 ###################################### Prüfen, ob die JSON-Daten erfolgreich dekodiert wurden
 ### folgender Block nicht zwingend notwendig, nur für Troubleshooting: Die rohen JSON-Daten in die Tabelle receiveddata einfügen
 
-// if (json_last_error() === JSON_ERROR_NONE && !empty($input)) {
-//     $sql = "INSERT INTO receiveddata (msg) VALUES (?)";
-//     $stmt = $pdo->prepare($sql);
-//     $stmt->execute([$inputJSON]);
-// }
+//if (json_last_error() === JSON_ERROR_NONE && !empty($input)) {
+//$sql = "INSERT INTO receiveddata (msg) VALUES (?)";
+//$stmt = $pdo->prepare($sql);
+//$stmt->execute([$inputJSON]);
+//}
 
-// echo "</br></br> Zeig die letzten 5 empfangenen HTTP Requests";
-// $sql = "SELECT * FROM receiveddata ORDER BY id DESC LIMIT 5";
-// $stmt = $pdo->prepare($sql);
-// $stmt->execute();
-// $receiveddata = $stmt->fetchAll();
+//echo "</br></br> Zeig die letzten 5 empfangenen HTTP Requests";
+//$sql = "SELECT * FROM receiveddata ORDER BY id DESC LIMIT 5";
+//$stmt = $pdo->prepare($sql);
+//$stmt->execute();
+//$receiveddata = $stmt->fetchAll();
 
-// echo "<ul>";
-// foreach ($receiveddata as $data) {
-//     echo "<li>" . $data['msg'] . "</li>";
-// }
-// echo "</ul>";
+//echo "<ul>";
+//foreach ($receiveddata as $data) {
+//echo "<li>" . $data['msg'] . "</li>";
+//}
+//echo "</ul>";
 
 
 ###################################### receiving a post request from a HTML form, later from ESP32 C6
@@ -60,6 +67,7 @@ $wert = $input["wert"];
 $sql = "INSERT INTO sensordata (wert) VALUES (?)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$wert]);
-// $stmt->execute(["42"]);
+//$stmt->execute(["42"]);
+
 
 ?>
